95edebc69b2a60c28e2bfea1970120e5490bfd86
Rajat <rajat.apptunix@gmail.com>
2020-08-12 19:20:49 +0530

    12-08-2020

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/contact-us', 'HomeController@contact')->name('contact-us');
 
+Route::prefix('auth')->name('auth.')->group(function() {
+    Route::get('facebook', 'Auth\RegisterController@facebookRedirect')->name('facebook');
+    Route::get('facebook/callback', 'Auth\RegisterController@facebookCallback');
+
+    Route::get('google', 'Auth\RegisterController@googleRedirect')->name('google');
+    Route::get('google/callback', 'Auth\RegisterController@googleCallback');
+});
